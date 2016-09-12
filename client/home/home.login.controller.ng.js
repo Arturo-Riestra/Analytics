66600fd76170dc39283315d869dac0d555a960f2
Arturo-Riestra <rhaftel@hotmail.com>
2016-09-12 18:29:52 -0500

    Log in
    Remember Me, log in validations

--- client/home/home.login.controller.ng.js
+++ client/home/home.login.controller.ng.js
@@ -3,15 +3,57 @@
 angular.module('analyticsApp')
 .controller('HomeLoginCtrl', function($scope, $state) {
 
-$scope.login = function() {
+$scope.loginFrame = true;
+$scope.requestFrame = false;
 
-  Meteor.loginWithPassword($scope.username, $scope.password, function(err){
-        if(err){
-            console.log(err)
-        } else {
-          $state.go('home');
-        }
-    });
+$scope.login = function() {
+  $scope.reason = "";
+
+  if ($scope.username === undefined || $scope.password === undefined) {
+
+    $scope.reason = "Credentials are required"
+  } else {
+    Meteor.loginWithPassword($scope.username, $scope.password, function(err){
+          if(err){
+              $scope.reason = err.reason;
+              console.log(err)
+          } else {
+            if (!$scope.rememberMe) {
+              Accounts._autoLoginEnabled = false;
+              Accounts._unstoreLoginToken();
+              var pollLoginState = function () {
+                var currentLoginToken = Accounts._storedLoginToken();
+                if (! currentLoginToken) return;
+
+                if (Accounts._lastLoginTokenWhenPolled != currentLoginToken) {
+                  if (currentLoginToken) {
+                    Accounts.loginWithToken(currentLoginToken, function (err) {
+                      if (err) {
+                        Accounts.makeClientLoggedOut();
+                      }
+                    });
+                  } else {
+                    Accounts.logout();
+                  }
+                }
+
+                Accounts._lastLoginTokenWhenPolled = currentLoginToken;
+              };
+
+              setInterval(function () {
+                pollLoginState();
+              }, 3000);
+
+            }
+
+            $state.go('home');
+          }
+      });
+  }
+
+
+
+
 };
 
 $scope.isLoggedIn = function() {
